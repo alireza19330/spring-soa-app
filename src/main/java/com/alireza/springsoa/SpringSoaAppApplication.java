9dd65136fc2c0a5209865445057b2a53bd1c8bd3
Alireza Abazari <alireza19330@gmail.com>
2018-05-29 21:26:36 +1000

    First Commit, basic spring-boot web app

---
+++ src/main/java/com/alireza/springsoa/SpringSoaAppApplication.java
@@ -0,0 +1,12 @@
+package com.alireza.springsoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoaAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alireza/springsoa/SpringSoaAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alireza.springsoa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSoaAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
