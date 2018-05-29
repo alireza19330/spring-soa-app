136313136b129a246c714c3bb855cd8170cf889c
Alireza Abazari <alireza19330@gmail.com>
2018-05-29 22:07:55 +1000

    Add simple unit test with Mock service

--- src/test/java/com/alireza/springsoa/SpringSoaAppApplicationTests.java
+++ src/test/java/com/alireza/springsoa/SpringSoaAppApplicationTests.java
@@ -2,15 +2,25 @@ package com.alireza.springsoa;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.alireza.springsoa.service.NotificationService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringSoaAppApplicationTests {
+	
+	@Mock
+	private NotificationService service2;
 
 	@Test
 	public void contextLoads() {
+		Mockito.when(service2.getLastNotification()).thenReturn("mynotify");
+		String lastNotification = service2.getLastNotification();
+		org.junit.Assert.assertNotNull(lastNotification);
 	}
 
 }
